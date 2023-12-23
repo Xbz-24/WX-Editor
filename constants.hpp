bd1892dfef81f908e4216027eda27983b27c373b
daily.dev <daily@waify.club>
2023-12-22 21:40:56 -0300

    added dark theme switcher

--- constants.hpp
+++ constants.hpp
@@ -1,15 +1,142 @@
 //
 // Created by daily on 21-12-23.
 //
-
 #ifndef EDITOR_CONSTANTS_HPP
 #define EDITOR_CONSTANTS_HPP
 
-#include <string>
+#include <wx/colour.h>
+#include <wx/string.h>
+#include <wx/gdicmn.h>
 
 namespace Constants
 {
-    const std::string editorKeywords = "return int float double char if else while for break continue switch case default";
+    // Lexer
+    const int LEXER_CPP = wxSTC_LEX_CPP;
+
+    // Color constants
+    const wxColour COLOR_STRING(150, 0 ,0);
+    const wxColour COLOR_PREPROCESSOR(165,105,0);
+    const wxColour COLOR_IDENTIFIER(40,0,60);
+    const wxColour COLOR_NUMBER(0,150,0);
+    const wxColour COLOR_CHARACTER(150,0,0);
+    const wxColour COLOR_WORD(0,0,150);
+    const wxColour COLOR_WORD2(0,150,0);
+    const wxColour COLOR_COMMENT(150,150,150);
+    const wxColour COLOR_COMMENT_LINE(150, 150, 150);
+    const wxColour COLOR_COMMENT_DOC(150, 150, 150);
+    const wxColour COLOR_OPERATOR(0,0,200);
+
+    // Editor keywords
+    const wxString EDITOR_KEYWORDS = "return int float double char if else while for break continue switch case default";
+    const wxString AUTO_COMP_KEYWORDS = "int char float double";
+
+    // Style constants
+    const bool STYLE_BOLD = true;
+
+    // Margin and marker constants
+    const int MARGIN_NUMBER_TYPE = wxSTC_MARGIN_NUMBER;
+    const int MARGIN_SYMBOL_TYPE = wxSTC_MARGIN_SYMBOL;
+    const int MARGIN_WIDTH = 16;
+    const int MARKER_FOLDER = wxSTC_MARKNUM_FOLDER;
+    const int MARKER_FOLDER_OPEN = wxSTC_MARKNUM_FOLDEROPEN;
+
+    // Button constants
+    const wxPoint SAVE_BUTTON_POSITION(10,300);
+    const wxPoint OPEN_BUTTON_POSITION(100,300);
+    const wxString SAVE_BUTTON_LABEL = "Save";
+    const wxString OPEN_BUTTON_LABEL = "Open";
+
+    namespace LightTheme
+    {
+        const wxColour COLOR_STRING(150, 0, 0);
+        const wxColour COLOR_PREPROCESSOR(165, 105, 0);
+        const wxColour COLOR_IDENTIFIER(40, 0, 60);
+        const wxColour COLOR_NUMBER(0, 150, 0);
+        const wxColour COLOR_CHARACTER(150, 0, 0);
+        const wxColour COLOR_WORD(0, 0, 150);
+        const wxColour COLOR_WORD2(0, 150, 0);
+        const wxColour COLOR_COMMENT(150, 150, 150);
+        const wxColour COLOR_COMMENT_LINE(150, 150, 150);
+        const wxColour COLOR_COMMENT_DOC(150, 150, 150);
+        const wxColour COLOR_OPERATOR(0, 0, 200);
+        const wxColour BACKGROUND(255, 255, 255); // white
+        const wxColour FOREGROUND(0, 0, 0); // black
+    };
+    namespace DarkTheme
+    {
+        const wxColour COLOR_STRING(139, 233, 253);
+        const wxColour COLOR_PREPROCESSOR(80, 250, 123);
+        const wxColour COLOR_IDENTIFIER(248, 248, 242);
+        const wxColour COLOR_NUMBER(189, 147, 249);
+        const wxColour COLOR_CHARACTER(255, 184, 108);
+        const wxColour COLOR_WORD(139, 233, 253);
+        const wxColour COLOR_WORD2(80, 250, 123);
+        const wxColour COLOR_COMMENT(98, 114, 164);
+        const wxColour COLOR_COMMENT_LINE(98, 114, 164);
+        const wxColour COLOR_COMMENT_DOC(98, 114, 164);
+        const wxColour COLOR_OPERATOR(248, 248, 242);
+        const wxColour BACKGROUND(40, 42, 54); // dark
+        const wxColour FOREGROUND(248, 248, 242); //white
+    };
+
+    struct ThemeSettings
+    {
+        wxColour colorString;
+        wxColour colorPreprocessor;
+        wxColour colorIdentifier;
+        wxColour colorNumber;
+        wxColour colorCharacter;
+        wxColour colorWord;
+        wxColour colorWord2;
+        wxColour colorComment;
+        wxColour colorCommentLine;
+        wxColour colorCommentDoc;
+        wxColour colorOperator;
+        wxColour background;
+        wxColour foreground;
+    };
+
+    inline ThemeSettings GetThemeSettings(bool isDarkMode)
+    {
+        if(!isDarkMode)
+        {
+            return ThemeSettings
+            {
+                    DarkTheme::COLOR_STRING,
+                    DarkTheme::COLOR_PREPROCESSOR,
+                    DarkTheme::COLOR_IDENTIFIER,
+                    DarkTheme::COLOR_NUMBER,
+                    DarkTheme::COLOR_CHARACTER,
+                    DarkTheme::COLOR_WORD,
+                    DarkTheme::COLOR_WORD2,
+                    DarkTheme::COLOR_COMMENT,
+                    DarkTheme::COLOR_COMMENT_LINE,
+                    DarkTheme::COLOR_COMMENT_DOC,
+                    DarkTheme::COLOR_OPERATOR,
+                    DarkTheme::BACKGROUND,
+                    DarkTheme::FOREGROUND
+            };
+        }
+        else
+        {
+            return ThemeSettings
+            {
+                    LightTheme::COLOR_STRING,
+                    LightTheme::COLOR_PREPROCESSOR,
+                    LightTheme::COLOR_IDENTIFIER,
+                    LightTheme::COLOR_NUMBER,
+                    LightTheme::COLOR_CHARACTER,
+                    LightTheme::COLOR_WORD,
+                    LightTheme::COLOR_WORD2,
+                    LightTheme::COLOR_COMMENT,
+                    LightTheme::COLOR_COMMENT_LINE,
+                    LightTheme::COLOR_COMMENT_DOC,
+                    LightTheme::COLOR_OPERATOR,
+                    LightTheme::BACKGROUND,
+                    LightTheme::FOREGROUND
+            };
+        }
+    }
 };
 
 #endif //EDITOR_CONSTANTS_HPP
--- main_frame.cpp
+++ main_frame.cpp
@@ -2,82 +2,200 @@
 #include <wx/textctrl.h>
 #include <wx/stc/stc.h>
 #include <wx/button.h>
+#include <wx/wfstream.h>
 #include "constants.hpp"
 
 class app : public wxApp
 {
-public:
-    bool OnInit() override;
+    public:
+        bool OnInit() override;
 };
 
-class main_editor_frame : public wxFrame
-{
+class main_editor_frame : public wxFrame {
 private:
-    wxStyledTextCtrl* editor;
-    wxButton* saveButton;
-    wxButton* openButton;
+    wxStyledTextCtrl *editor;
+    wxButton *saveButton;
+    wxButton *openButton;
+    wxButton *newFileButton;
+    wxButton *toggleDarkModeButton;
 
 public:
-    main_editor_frame(const wxString& title, const wxPoint& pos, const wxSize& size);
-    void OnSave(wxCommandEvent& event);
-    void OnOpen(wxCommandEvent& event);
+    main_editor_frame(const wxString &title, const wxPoint &pos, const wxSize &size);
+
+    void OnSave(wxCommandEvent &event);
+
+    void OnOpen(wxCommandEvent &event);
+
+    void OnNewFile(wxCommandEvent &event);
+
+    void OnToggleDarkMode(wxCommandEvent &event);
+
+    void ApplyEditorStyles(bool isDarkMode);
 };
 
 wxIMPLEMENT_APP(app);
 
 bool app::OnInit()
 {
-    auto *frame = new main_editor_frame("text-editor_wx-widgets", wxPoint(50, 50), wxSize(450, 340));
+    wxFrame *frame = new main_editor_frame("wx-editor", wxPoint(50, 50), wxSize(450, 340));
+    frame->Maximize();
     frame->Show(true);
     return true;
 }
 
-main_editor_frame::main_editor_frame(const wxString& title, const wxPoint& pos, const wxSize& size): wxFrame(nullptr, wxID_ANY, title, pos, size)
+main_editor_frame::main_editor_frame(const wxString& title, const wxPoint& pos, const wxSize& size)
+                            :wxFrame(nullptr, wxID_ANY, title, pos, size)
 {
+    auto* vbox = new wxBoxSizer(wxVERTICAL);
+
+    saveButton = new wxButton(this, wxID_ANY, Constants::SAVE_BUTTON_LABEL, Constants::SAVE_BUTTON_POSITION, wxDefaultSize);
+    openButton = new wxButton(this, wxID_ANY, Constants::OPEN_BUTTON_LABEL, Constants::OPEN_BUTTON_POSITION, wxDefaultSize);
+    newFileButton = new wxButton(this, wxID_ANY, "New File");
+    toggleDarkModeButton = new wxButton(this, wxID_ANY, "Toggle Dark Mode");
+
+    saveButton -> Bind(wxEVT_BUTTON, &main_editor_frame::OnSave, this);
+    openButton -> Bind(wxEVT_BUTTON, &main_editor_frame::OnOpen, this);
+    newFileButton -> Bind(wxEVT_BUTTON, &main_editor_frame::OnNewFile, this);
+    toggleDarkModeButton -> Bind(wxEVT_BUTTON, &main_editor_frame::OnToggleDarkMode, this);
+
+    auto* hbox = new wxBoxSizer(wxHORIZONTAL);
+    hbox->Add(saveButton);
+    hbox->Add(openButton);
+    hbox->Add(newFileButton);
+    hbox->Add(toggleDarkModeButton);
+
+    vbox->Add(hbox,0,wxEXPAND | wxALL, 5);
     editor = new wxStyledTextCtrl(this, wxID_ANY);
-    editor->SetLexer(wxSTC_LEX_CPP);
-    editor->StyleSetForeground(wxSTC_C_STRING, wxColour(150, 0, 0));
-    editor->StyleSetForeground(wxSTC_C_PREPROCESSOR, wxColour(165, 105, 0));
-    editor->StyleSetForeground(wxSTC_C_IDENTIFIER, wxColour(40, 0, 60));
-    editor->StyleSetForeground(wxSTC_C_NUMBER, wxColour(0, 150, 0));
-    editor->StyleSetForeground(wxSTC_C_CHARACTER, wxColour(150, 0, 0));
-    editor->StyleSetForeground(wxSTC_C_WORD, wxColour(0, 0, 150));
-    editor->StyleSetForeground(wxSTC_C_WORD2, wxColour(0, 150, 0));
-    editor->StyleSetForeground(wxSTC_C_COMMENT, wxColour(150, 150, 150));
-    editor->StyleSetForeground(wxSTC_C_COMMENTLINE, wxColour(150, 150, 150));
-    editor->StyleSetForeground(wxSTC_C_COMMENTDOC, wxColour(150, 150, 150));
-    editor->StyleSetForeground(wxSTC_C_OPERATOR, wxColour(0,0,200));
-    editor->StyleSetBold(wxSTC_C_WORD, true);
-    editor->StyleSetBold(wxSTC_C_WORD2, true);
-    editor->StyleSetBold(wxSTC_C_COMMENTDOC, true);
-    editor->SetKeyWords(0,Constants::editorKeywords);
-    editor->SetMarginType(0, wxSTC_MARGIN_NUMBER);
+
+    editor->SetLexer(Constants::LEXER_CPP);
+
+    editor->StyleSetForeground(wxSTC_C_STRING       , Constants::COLOR_STRING);
+    editor->StyleSetForeground(wxSTC_C_PREPROCESSOR , Constants::COLOR_PREPROCESSOR);
+    editor->StyleSetForeground(wxSTC_C_IDENTIFIER   , Constants::COLOR_IDENTIFIER);
+    editor->StyleSetForeground(wxSTC_C_NUMBER       , Constants::COLOR_NUMBER);
+    editor->StyleSetForeground(wxSTC_C_CHARACTER    , Constants::COLOR_CHARACTER);
+    editor->StyleSetForeground(wxSTC_C_WORD         , Constants::COLOR_WORD);
+    editor->StyleSetForeground(wxSTC_C_WORD2        , Constants::COLOR_WORD2);
+    editor->StyleSetForeground(wxSTC_C_COMMENT      , Constants::COLOR_COMMENT);
+    editor->StyleSetForeground(wxSTC_C_COMMENTLINE  , Constants::COLOR_COMMENT_LINE);
+    editor->StyleSetForeground(wxSTC_C_COMMENTDOC   , Constants::COLOR_COMMENT_DOC);
+    editor->StyleSetForeground(wxSTC_C_OPERATOR     , Constants::COLOR_OPERATOR);
+
+    editor->StyleSetBold(wxSTC_C_WORD               , Constants::STYLE_BOLD);
+    editor->StyleSetBold(wxSTC_C_WORD2              , Constants::STYLE_BOLD);
+    editor->StyleSetBold(wxSTC_C_COMMENTDOC         , Constants::STYLE_BOLD);
+
+    editor->SetKeyWords(0,Constants::EDITOR_KEYWORDS);
+    editor->SetMarginType(0, Constants::MARGIN_NUMBER_TYPE);
     editor->SetMarginWidth(0, editor->TextWidth(wxSTC_STYLE_LINENUMBER, "_99999"));
-    editor->SetMarginType(1, wxSTC_MARGIN_SYMBOL);
-    editor->SetMarginType(1, wxSTC_MASK_FOLDERS);
-    editor->SetMarginWidth(1,16);
+    editor->SetMarginType(1, Constants::MARGIN_SYMBOL_TYPE);
+    editor->SetMarginWidth(1,Constants::MARGIN_WIDTH);
     editor->SetMarginSensitive(1,true);
-    editor->MarkerDefine(wxSTC_MARKNUM_FOLDER, wxSTC_MARK_BOXPLUS);
-    editor->MarkerDefine(wxSTC_MARKNUM_FOLDEROPEN, wxSTC_MARK_BOXMINUS);
+
+    editor->MarkerDefine(Constants::MARKER_FOLDER       , wxSTC_MARK_BOXPLUS);
+    editor->MarkerDefine(Constants::MARKER_FOLDER_OPEN  , wxSTC_MARK_BOXMINUS);
+
     editor->SetProperty("fold", "1");
     editor->SetFoldFlags(wxSTC_FOLDFLAG_LINEBEFORE_CONTRACTED | wxSTC_FOLDFLAG_LINEAFTER_CONTRACTED);
+
     editor->AutoCompSetSeparator(' ');
     editor->AutoCompSetIgnoreCase(true);
     editor->AutoCompSetAutoHide(false);
     editor->AutoCompSetDropRestOfWord(true);
-    editor->AutoCompShow(0, "int char float double");
-    saveButton = new wxButton(this, wxID_ANY, wxT("Save"), wxPoint(10, 300), wxDefaultSize);
-    openButton = new wxButton(this, wxID_ANY, wxT("Open"), wxPoint(100, 300), wxDefaultSize);
-    saveButton -> Bind(wxEVT_BUTTON, &main_editor_frame::OnSave, this);
-    openButton -> Bind(wxEVT_BUTTON, &main_editor_frame::OnOpen, this);
+
+    editor->AutoCompShow(0, Constants::AUTO_COMP_KEYWORDS);
+
+    vbox->Add(editor,1, wxEXPAND);
+    this->SetSizer(vbox);
+    this->Layout();
 }
 
 void main_editor_frame::OnSave(wxCommandEvent &event)
 {
+    wxFileDialog saveFileDialog
+    (
+    this, _("Save File"), "", "",
+         "Text Files (*.txt)|*.txt|All files (*.*)|*.*",
+         wxFD_SAVE | wxFD_OVERWRITE_PROMPT
+    );
 
+    if(saveFileDialog.ShowModal() == wxID_CANCEL)
+    {
+        return;
+    }
+    if(!editor->SaveFile(saveFileDialog.GetPath()))
+    {
+        wxLogError("Cannot save current contents in file '%s'.", saveFileDialog.GetPath());
+    }
 }
 
 void main_editor_frame::OnOpen(wxCommandEvent &event)
 {
+    wxFileDialog openFileDialog
+    (
+     this, _("Open file"), "", "",
+    "Text Files (*.txt)|*.txt|All files (*.*)|*.*",
+     wxFD_OPEN|wxFD_FILE_MUST_EXIST
+    );
 
+    if(openFileDialog.ShowModal() == wxID_CANCEL)
+    {
+        return;
+    }
+
+    wxFileInputStream input_stream(openFileDialog.GetPath());
+    if(!input_stream.IsOk())
+    {
+        wxLogError("Cannot open file '%s'.", openFileDialog.GetPath());
+        return;
+    }
+    editor->LoadFile(openFileDialog.GetPath());
+}
+
+void main_editor_frame::OnNewFile(wxCommandEvent& event)
+{
+    editor->ClearAll();
+}
+
+void main_editor_frame::OnToggleDarkMode(wxCommandEvent& event)
+{
+    static bool isDarkMode = false;
+    ApplyEditorStyles(isDarkMode);
+    isDarkMode = !isDarkMode;
+}
+
+void main_editor_frame::ApplyEditorStyles(bool isDarkMode)
+{
+    Constants::ThemeSettings theme = Constants::GetThemeSettings(isDarkMode);
+
+    editor->StyleClearAll();
+
+    editor->SetLexer(Constants::LEXER_CPP);
+    editor->SetKeyWords(0, Constants::EDITOR_KEYWORDS);
+
+    for(int style = 0; style < wxSTC_STYLE_LASTPREDEFINED; style++)
+    {
+        editor->StyleSetBackground(style, theme.background);
+        editor->StyleSetForeground(style, theme.foreground);
+    }
+
+    editor->StyleSetForeground(wxSTC_C_STRING, theme.colorString);
+    editor->StyleSetForeground(wxSTC_C_PREPROCESSOR, theme.colorPreprocessor);
+    editor->StyleSetForeground(wxSTC_C_IDENTIFIER, theme.colorIdentifier);
+    editor->StyleSetForeground(wxSTC_C_NUMBER, theme.colorNumber);
+    editor->StyleSetForeground(wxSTC_C_CHARACTER, theme.colorCharacter);
+    editor->StyleSetForeground(wxSTC_C_WORD, theme.colorWord);
+    editor->StyleSetForeground(wxSTC_C_WORD2, theme.colorWord2);
+    editor->StyleSetForeground(wxSTC_C_COMMENT, theme.colorComment);
+    editor->StyleSetForeground(wxSTC_C_COMMENTLINE, theme.colorCommentLine);
+    editor->StyleSetForeground(wxSTC_C_COMMENTDOC, theme.colorCommentDoc);
+    editor->StyleSetForeground(wxSTC_C_OPERATOR, theme.colorOperator);
+
+    if (Constants::STYLE_BOLD)
+    {
+        editor->StyleSetBold(wxSTC_C_WORD, true);
+        editor->StyleSetBold(wxSTC_C_WORD2, true);
+    }
+    editor->Refresh();
+    editor->Update();
 }
