12c65b991664c395940a71b066f3cb97e550d61c
daily <daily@waifu.club>
2024-02-10 22:37:32 -0300

    added documentation to StatusBarComponent.cpp

--- StatusBarComponent.cpp
+++ StatusBarComponent.cpp
@@ -1,7 +1,12 @@
-//
-// Created by daily on 1/31/24.
-//
+/**
+ * @file LayoutComponent.hpp
+ * @brief This class is responsible for setting up the layout of the editor.
+ * @version 0.1
+ * @date 2024-01-31
+ * @Author Renato German Chavez Chicoma
+ */
 #include "StatusBarComponent.hpp"
+
 StatusBarComponent::StatusBarComponent(wxWindow *parent)
 {
 }
